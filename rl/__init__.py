58b990dd2a872b503fe3e4d1e8119dd30bf96076
Stefan H. Holek <stefan@epy.co.at>
2009-08-30 11:33:38 +0200

    Goodbye _utils.py.

--- rl/__init__.py
+++ rl/__init__.py
@@ -4,10 +4,9 @@ from _stdlib import readline # [sic]
 import _readline as readline
 
 from _completer import completer
+from _completer import generator
 from _completion import completion
 from _history import history
-from _utils import generator
-from _utils import print_exc
 
 # For subclassing
 from _completer import Completer
@@ -16,3 +15,23 @@ from _history import History
 
 # Patch cmd.Cmd to use rl instead of readline
 import _cmd
+
+# Debugging helper
+def print_exc(func):
+    """Decorator printing exceptions to stderr.
+
+    Useful when debugging completions and hooks, as exceptions occurring
+    there are usually swallowed by the in-between C code.
+    """
+
+    def wrapped_func(*args, **kw):
+        try:
+            return func(*args, **kw)
+        except:
+            import traceback; traceback.print_exc()
+            raise
+
+    wrapped_func.__name__ = func.__name__
+    wrapped_func.__dict__ = func.__dict__
+    wrapped_func.__doc__ = func.__doc__
+    return wrapped_func
