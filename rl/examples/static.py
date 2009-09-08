734470b1f00e393d53682f24ab8c9f3e306152d8
Stefan H. Holek <stefan@epy.co.at>
2009-09-08 21:50:54 +0200

    Complete from list of strings.

---
+++ rl/examples/static.py
@@ -0,0 +1,29 @@
+# Complete from a static list of strings
+
+from rl import completer
+from rl import completion
+from rl import generator
+from rl import print_exc
+
+strings = ['foo-', 'bar-', 'baz+']
+
+
+@print_exc
+def complete(text):
+    completion.suppress_append = True
+    return [x for x in strings if x.startswith(text)]
+
+
+def main():
+    # Set the completion function
+    completer.completer = generator(complete)
+
+    # Enable TAB completion
+    completer.parse_and_bind('tab: complete')
+
+    line = raw_input('> ')
+    print 'You typed:', line
+
+
+if __name__ == '__main__':
+    main()
