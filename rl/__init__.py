7d4303c89852df3dc3eb1294da63cf79063affaa
Stefan H. Holek <stefan@epy.co.at>
2009-09-08 21:34:01 +0200

    Add example.

--- rl/__init__.py
+++ rl/__init__.py
@@ -1,4 +1,21 @@
-"""Alternative readline interface focusing on completion."""
+"""Alternative readline interface focusing on completion.
+
+Example::
+
+    from rl import completer
+    from rl import completion
+    from rl import generator
+
+    strings = ['foo-', 'bar-', 'baz+']
+
+    def complete(text):
+        completion.suppress_append = True
+        return [x for x in strings if x.startswith(text)]
+
+    completer.completer = generator(complete)
+    completer.parse_and_bind('tab: complete')
+    line = raw_input('> ')
+"""
 
 # Make sure we can grab the ReadlineFunctionPointer
 from stdlib import readline
@@ -7,6 +24,7 @@ import readline
 # Patch cmd.Cmd to use rl instead of readline
 import cmd
 
+# This makes epydoc cry
 from completion import completer
 from completion import completion
 from completion import generator
