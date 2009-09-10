de29d94e2bfc25d781f3ea7f94eddd0a1d3b2150
Stefan H. Holek <stefan@epy.co.at>
2009-09-10 09:22:22 +0200

    How did this get reverted?

--- rl/_completion.py
+++ rl/_completion.py
@@ -94,7 +94,7 @@ class Completer(object):
         doc="""Up to this many items will be displayed in response to a
         possible-completions call. Beyond that the user is prompted if he
         really wants to see all matches. Defaults to 100. A negative value
-        means never prompt. The prompt is bypassed when a custom
+        means never prompt. The prompt is bypassed if a custom
         ``display_matches_hook`` is installed."""
         def get(self):
             return readline.get_completion_query_items()
