97bc670bbd27f47177eb79ecf0b05f1e97076d58
Stefan H. Holek <stefan@epy.co.at>
2009-10-31 01:51:16 +0100

    Move redisplay before read_key in completion interface.

--- rl/_completion.py
+++ rl/_completion.py
@@ -420,13 +420,13 @@ class Completion(object):
         """Built-in matches display."""
         readline.display_match_list(substitution, matches, longest_match_length)
 
-    def read_key(self):
-        """Read a key from readline's input stream (the keyboard)."""
-        return readline.read_key()
-
     def redisplay(self, force=False):
         """Refresh what's displayed on the screen."""
         readline.redisplay(force)
+
+    def read_key(self):
+        """Read a key from readline's input stream (the keyboard)."""
+        return readline.read_key()
 
     def _generate(self, text, entry_func):
         """Extract a list of matches from a generator function."""
