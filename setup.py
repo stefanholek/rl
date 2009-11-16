7c213d47759853b4afad4dc471daa5632936cbe0
Stefan H. Holek <stefan@epy.co.at>
2009-11-16 16:15:43 +0100

    Post-release version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, Extension
 from os.path import join, exists
 from sys import platform
 
-version = '1.1'
+version = '1.2'
 
 
 # On Linux, install libreadline5-dev (or equivalent) before attempting to
