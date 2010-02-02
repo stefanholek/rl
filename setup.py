2b9b7b473bb7de63563c41abe438e23ab0749452
Stefan H. Holek <stefan@epy.co.at>
2010-02-02 18:11:39 +0100

    Build a static readline-6.1 with distutils.

--- setup.py
+++ setup.py
@@ -1,37 +1,103 @@
+import sys
+import os
+
 from setuptools import setup, find_packages, Extension
 from os.path import join, exists
-from sys import platform
 
-version = '1.4'
-
-
-# On Linux, install libreadline5-dev (or equivalent) before attempting to
-# build rl. On Mac OS X, you need a Python built with MacPorts or Fink,
-# as the system Python is linked to the BSD editline library and not
-# GNU readline.
-
-include_dirs = []
-library_dirs = []
-
-libraries = ['readline', 'ncurses']
-
-if platform == 'darwin':
-    # MacPorts
-    if exists('/opt/local/include'):
-        include_dirs = ['/opt/local/include']
-    # Fink
-    elif exists('/sw/include'):
-        include_dirs = ['/sw/include']
-
-    libraries = ['readline', 'ncursesw']
+version = '2.0'
 
 
-readline = \
+# Builds rl.readline with a static readline-6.1.
+
+sources = [
+    'build/readline/bind.c',
+    'build/readline/callback.c',
+    'build/readline/compat.c',
+    'build/readline/complete.c',
+    'build/readline/display.c',
+    'build/readline/funmap.c',
+    'build/readline/histexpand.c',
+    'build/readline/histfile.c',
+    'build/readline/history.c',
+    'build/readline/histsearch.c',
+    'build/readline/input.c',
+    'build/readline/isearch.c',
+    'build/readline/keymaps.c',
+    'build/readline/kill.c',
+    'build/readline/macro.c',
+    'build/readline/mbutil.c',
+    'build/readline/misc.c',
+    'build/readline/nls.c',
+    'build/readline/parens.c',
+    'build/readline/readline.c',
+    'build/readline/rltty.c',
+    'build/readline/savestring.c',
+    'build/readline/search.c',
+    'build/readline/shell.c',
+    'build/readline/signals.c',
+    'build/readline/terminal.c',
+    'build/readline/text.c',
+    'build/readline/tilde.c',
+    'build/readline/undo.c',
+    'build/readline/util.c',
+    'build/readline/vi_mode.c',
+    'build/readline/xmalloc.c',
+    'rl/readline.c',
+]
+
+define_macros = [
+    ('HAVE_CONFIG_H', None),
+    ('RL_LIBRARY_VERSION', '"6.1"'),
+    ('HAVE_RL_CALLBACK', None),
+    ('HAVE_RL_CATCH_SIGNAL', None),
+    ('HAVE_RL_COMPLETION_APPEND_CHARACTER', None),
+    ('HAVE_RL_COMPLETION_DISPLAY_MATCHES_HOOK', None),
+    ('HAVE_RL_COMPLETION_MATCHES', None),
+    ('HAVE_RL_COMPLETION_SUPPRESS_APPEND', None),
+    ('HAVE_RL_PRE_INPUT_HOOK', None),
+]
+
+include_dirs = ['build', 'build/readline']
+libraries = ['ncurses']
+extra_compile_args = []
+extra_link_args = []
+
+if sys.platform == 'darwin':
+    extra_compile_args.extend([
+        '-Wno-all',
+        '-Wno-strict-prototypes',
+    ])
+
+
+configure = False
+quiet = ''
+
+for arg in sys.argv[1:]:
+    if arg.startswith(('bdist', 'build', 'develop', 'test', 'install')):
+        configure = True
+    if arg in ('-q', '--quiet'):
+        quiet = '>' + os.devnull
+
+if configure and not exists(join('build', 'readline', 'config.h')):
+    os.system("""\
+    mkdir -p build
+    cd build
+    rm -rf readline-6.1 readline
+    tar xzf ../thirdparty/readline-6.1.tar.gz
+    mv readline-6.1 readline
+    cd readline
+    ./configure %(quiet)s
+    """ % locals())
+
+
+rl_readline = \
 Extension(name='rl.readline',
-          sources=[join('rl', 'readline.c')],
+          sources=sources,
+          define_macros=define_macros,
           include_dirs=include_dirs,
-          library_dirs=library_dirs,
           libraries=libraries,
+          extra_compile_args=extra_compile_args,
+          extra_link_args=extra_link_args,
 )
 
 
@@ -43,24 +109,24 @@ setup(name='rl',
       classifiers=[
           'Programming Language :: Python',
           'Programming Language :: C',
-          'Operating System :: Unix',
           'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX',
           'Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: Python Software Foundation License',
-          'License :: OSI Approved :: BSD License',
+          'License :: OSI Approved :: GNU General Public License',
       ],
       keywords='gnu readline completion interface',
       author='Stefan H. Holek',
       author_email='stefan@epy.co.at',
       url='http://pypi.python.org/pypi/rl',
-      license='PSF or BSD',
+      license='PSF or GPL',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       use_2to3=True,
       test_suite='rl.tests',
       ext_modules=[
-          readline,
+          rl_readline,
       ],
       install_requires=[
           'setuptools',
