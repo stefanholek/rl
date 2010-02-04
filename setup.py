b7704223ef57fddf8d3777ac43cd7288e735c29e
Stefan H. Holek <stefan@epy.co.at>
2010-02-04 13:58:08 +0100

    Download and build a static readline-6.1 when installed into the Mac OS X system Python.

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+# On Linux, install libreadline5-dev (or equivalent) before attempting
+# to build rl. On Mac OS X, make sure you have XCode Tools installed.
+
 import sys
 import os
 
@@ -6,80 +9,109 @@ from os.path import join, exists
 
 version = '2.0'
 
-
-# Builds rl.readline with a static readline-6.1.
-
-sources = [
-    'build/readline/bind.c',
-    'build/readline/callback.c',
-    'build/readline/compat.c',
-    'build/readline/complete.c',
-    'build/readline/display.c',
-    'build/readline/funmap.c',
-    'build/readline/histexpand.c',
-    'build/readline/histfile.c',
-    'build/readline/history.c',
-    'build/readline/histsearch.c',
-    'build/readline/input.c',
-    'build/readline/isearch.c',
-    'build/readline/keymaps.c',
-    'build/readline/kill.c',
-    'build/readline/macro.c',
-    'build/readline/mbutil.c',
-    'build/readline/misc.c',
-    'build/readline/nls.c',
-    'build/readline/parens.c',
-    'build/readline/readline.c',
-    'build/readline/rltty.c',
-    'build/readline/savestring.c',
-    'build/readline/search.c',
-    'build/readline/shell.c',
-    'build/readline/signals.c',
-    'build/readline/terminal.c',
-    'build/readline/text.c',
-    'build/readline/tilde.c',
-    'build/readline/undo.c',
-    'build/readline/util.c',
-    'build/readline/vi_mode.c',
-    'build/readline/xmalloc.c',
-    'rl/readline.c',
-]
-
-define_macros = [
-    ('HAVE_CONFIG_H', None),
-    ('RL_LIBRARY_VERSION', '"6.1"'),
-    ('HAVE_RL_CALLBACK', None),
-    ('HAVE_RL_CATCH_SIGNAL', None),
-    ('HAVE_RL_COMPLETION_APPEND_CHARACTER', None),
-    ('HAVE_RL_COMPLETION_DISPLAY_MATCHES_HOOK', None),
-    ('HAVE_RL_COMPLETION_MATCHES', None),
-    ('HAVE_RL_COMPLETION_SUPPRESS_APPEND', None),
-    ('HAVE_RL_PRE_INPUT_HOOK', None),
-]
-
-include_dirs = ['build', 'build/readline']
-libraries = ['ncurses']
+sources = ['rl/readline.c']
+define_macros = []
+include_dirs = []
+libraries = ['readline', 'ncurses']
+extra_compile_args = []
 
 
-configure = False
-quiet = ''
+def system_python():
+    for dir in sys.path:
+        if dir.startswith('/System/Library/Frameworks/Python.framework'):
+            return True
 
-for arg in sys.argv[1:]:
-    if arg.startswith(('bdist', 'build', 'develop', 'test', 'install')):
-        configure = True
-    if arg in ('-q', '--quiet'):
-        quiet = '>' + os.devnull
 
-if configure and not exists(join('build', 'readline', 'config.h')):
-    os.system("""\
-    mkdir -p build
-    cd build
-    rm -rf readline-6.1 readline
-    tar xzf ../thirdparty/readline-6.1.tar.gz
-    mv readline-6.1 readline
-    cd readline
-    ./configure %(quiet)s
-    """ % locals())
+def use_static_readline():
+    sources.extend([
+        'build/readline/bind.c',
+        'build/readline/callback.c',
+        'build/readline/compat.c',
+        'build/readline/complete.c',
+        'build/readline/display.c',
+        'build/readline/funmap.c',
+        'build/readline/histexpand.c',
+        'build/readline/histfile.c',
+        'build/readline/history.c',
+        'build/readline/histsearch.c',
+        'build/readline/input.c',
+        'build/readline/isearch.c',
+        'build/readline/keymaps.c',
+        'build/readline/kill.c',
+        'build/readline/macro.c',
+        'build/readline/mbutil.c',
+        'build/readline/misc.c',
+        'build/readline/nls.c',
+        'build/readline/parens.c',
+        'build/readline/readline.c',
+        'build/readline/rltty.c',
+        'build/readline/savestring.c',
+        'build/readline/search.c',
+        'build/readline/shell.c',
+        'build/readline/signals.c',
+        'build/readline/terminal.c',
+        'build/readline/text.c',
+        'build/readline/tilde.c',
+        'build/readline/undo.c',
+        'build/readline/util.c',
+        'build/readline/vi_mode.c',
+        'build/readline/xmalloc.c',
+    ])
+
+    define_macros.extend([
+        ('HAVE_CONFIG_H', None),
+        ('RL_LIBRARY_VERSION', '"6.1"'),
+        ('HAVE_RL_CALLBACK', None),
+        ('HAVE_RL_CATCH_SIGNAL', None),
+        ('HAVE_RL_COMPLETION_APPEND_CHARACTER', None),
+        ('HAVE_RL_COMPLETION_DISPLAY_MATCHES_HOOK', None),
+        ('HAVE_RL_COMPLETION_MATCHES', None),
+        ('HAVE_RL_COMPLETION_SUPPRESS_APPEND', None),
+        ('HAVE_RL_PRE_INPUT_HOOK', None),
+    ])
+
+    include_dirs.extend(['build', 'build/readline'])
+    libraries.remove('readline')
+
+    if sys.platform == 'darwin':
+        extra_compile_args.extend(['-Wno-all', '-Wno-strict-prototypes'])
+
+    configure = False
+    quiet = ''
+
+    for arg in sys.argv[1:]:
+        if arg.startswith(('bdist', 'build', 'develop', 'test', 'install')):
+            configure = True
+        if arg in ('-q', '--quiet'):
+            quiet = '>' + os.devnull
+
+    if configure and not exists(join('build', 'readline', 'config.h')):
+        url = 'ftp://ftp.cwru.edu/pub/bash/readline-6.1.tar.gz'
+        os.system("""\
+        mkdir -p build
+        cd build
+        rm -rf readline-6.1 readline
+        echo Fetching readline-6.1 from %(url)s
+        curl --connect-timeout 30 -s %(url)s | tar xz
+        test $? -eq 0 || exit
+        mv readline-6.1 readline
+        cd readline
+        ./configure %(quiet)s
+        """ % locals())
+
+
+if sys.platform == 'darwin':
+    # System
+    if system_python():
+        use_static_readline()
+    # MacPorts
+    elif exists('/opt/local/include'):
+        include_dirs = ['/opt/local/include']
+        libraries = ['readline', 'ncursesw']
+    # Fink
+    elif exists('/sw/include'):
+        include_dirs = ['/sw/include']
+        libraries = ['readline', 'ncursesw']
 
 
 rl_readline = \
@@ -88,6 +120,7 @@ Extension(name='rl.readline',
           define_macros=define_macros,
           include_dirs=include_dirs,
           libraries=libraries,
+          extra_compile_args=extra_compile_args,
 )
 
 
