7c6606af194c66b3e71ffbb30f6f269c4739f0f0
Stefan H. Holek <stefan@epy.co.at>
2010-03-02 15:16:05 +0100

    Move macros to the end as they're not related to Unicode per se.

--- rl/readlineutils.h
+++ rl/readlineutils.h
@@ -8,14 +8,10 @@ char **StringArray_new(size_t size);
 void StringArray_free(char **strings);
 size_t StringArray_size(char **strings);
 int StringArray_insert(char ***strings, size_t pos, char *string);
-
 PyObject *PyList_FromStringArray(char **strings);
 char **StringArray_FromPyList(PyObject *list);
 
 #if (PY_MAJOR_VERSION >= 3)
-#define PyInt_FromLong PyLong_FromLong
-#define PyInt_AsLong PyLong_AsLong
-#define PyString_FromString PyUnicode_DECODE
 
 /* Unicode support */
 PyObject *PyUnicode_DECODE(const char *text);
@@ -23,6 +19,12 @@ PyObject *PyUnicode_DECODE_CHAR(char character);
 Py_ssize_t PyUnicode_AdjustIndex(const char *text, Py_ssize_t index);
 PyObject *PyUnicode_ENCODE(PyObject *text);
 int PyUnicode_StrConverter(PyObject *text, void *addr);
+
+/* Python 3 compatibility */
+#define PyInt_FromLong PyLong_FromLong
+#define PyInt_AsLong PyLong_AsLong
+#define PyString_FromString PyUnicode_DECODE
+
 #endif
 
 #endif /* __READLINEUTILS_H__ */
