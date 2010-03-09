0f1d901d3345359d48cc50426b62bb15bc898ad6
Stefan H. Holek <stefan@epy.co.at>
2010-03-09 21:34:47 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 from setuptools import setup, find_packages, Extension
 from os.path import join, exists
 
-version = '1.7'
+version = '1.8'
 
 sources = ['rl/readline.c', 'rl/readlineutils.c']
 define_macros = []
