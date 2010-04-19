61ad54466c298d2e412e008939dcc9488a2d4ab8
Stefan H. Holek <stefan@epy.co.at>
2010-04-19 21:28:42 +0200

    Improve history documentation.

--- rl/_history.py
+++ rl/_history.py
@@ -29,8 +29,8 @@ class History(object):
 
     @apply
     def max_entries():
-        doc="""The maximum number of history entries kept. Beyond this point
-        the history list is truncated by removing the oldest entry.
+        doc="""The maximum number of history entries kept. Beyond this
+        point the history list is truncated by removing the oldest entry.
         A negative value means no limit. Defaults to -1."""
         def get(self):
             if readline.history_is_stifled():
@@ -42,10 +42,6 @@ class History(object):
             else:
                 readline.stifle_history(int)
         return property(get, set, doc=doc)
-
-    def __len__(self):
-        """The current history length."""
-        return readline.get_current_history_length()
 
     def clear(self):
         """Clear the history."""
@@ -67,6 +63,10 @@ class History(object):
         """Replace the history item at index."""
         readline.replace_history_item(self._norm_index(index), line)
 
+    def __len__(self):
+        """The current history length."""
+        return readline.get_current_history_length()
+
     def __iter__(self):
         """Iterate over history items."""
         return iter(readline.get_history_list())
@@ -74,6 +74,14 @@ class History(object):
     def __reversed__(self):
         """Reverse-iterate over history items."""
         return reversed(readline.get_history_list())
+
+    def read_file(self, filename=None, raise_exc=False):
+        """Load a readline history file. The default filename is ~/.history."""
+        self._file_op(readline.read_history_file, filename, raise_exc)
+
+    def write_file(self, filename=None, raise_exc=False):
+        """Save a readline history file. The default filename is ~/.history."""
+        self._file_op(readline.write_history_file, filename, raise_exc)
 
     def _norm_index(self, index):
         """Support negative indexes."""
@@ -84,14 +92,6 @@ class History(object):
         if index < 0 or index >= len(self):
             raise IndexError('history index out of range')
         return index
-
-    def read_file(self, filename=None, raise_exc=False):
-        """Load a readline history file. The default filename is ~/.history."""
-        self._file_op(readline.read_history_file, filename, raise_exc)
-
-    def write_file(self, filename=None, raise_exc=False):
-        """Save a readline history file. The default filename is ~/.history."""
-        self._file_op(readline.write_history_file, filename, raise_exc)
 
     def _file_op(self, op, filename, raise_exc):
         """Perform a file operation optionally suppressing IOErrors."""
