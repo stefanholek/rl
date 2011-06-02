6c5093ba5c53e0fdc68df3b37a28d81fd6e3d8f6
Stefan H. Holek <stefan@epy.co.at>
2011-06-02 16:34:18 +0200

    Code cosmetics.

--- rl/readline.c
+++ rl/readline.c
@@ -1198,7 +1198,7 @@ set_filename_quoting_function(PyObject *self, PyObject *args)
 	rl_filename_quoting_function =
 		filename_quoting_function ?
 		(rl_quote_func_t *)on_filename_quoting_function :
-		default_filename_quoting_function;
+		(rl_quote_func_t *)default_filename_quoting_function;
 
 	return result;
 }
