f054ddc0db2c3a585c9eaaa822dec15151cf28ea
Stefan H. Holek <stefan@epy.co.at>
2011-08-28 02:18:26 +0200

    Add tests for PyUnicode_FSOrNoneConverter.

--- rl/tests/test_readline.py
+++ rl/tests/test_readline.py
@@ -1,9 +1,33 @@
 import unittest
+import sys
+import os
+
+from os.path import isfile, expanduser
 
 from rl import readline
+from rl.testing import reset
+from rl.testing import JailSetup
 
 
-class ReadlineTests(unittest.TestCase):
+class ReadlineTests(JailSetup):
+    # XXX You will lose your ~/.history file when you run these tests
+
+    def setUp(self):
+        JailSetup.setUp(self)
+        reset()
+        self.histfile = expanduser('~/.history')
+        self.initfile = expanduser('~/.$$$')
+        self.remove_files()
+
+    def tearDown(self):
+        self.remove_files()
+        JailSetup.tearDown(self)
+
+    def remove_files(self):
+        if isfile(self.histfile):
+            os.remove(self.histfile)
+        if isfile(self.initfile):
+            os.remove(self.initfile)
 
     def test_readline_version(self):
         self.assertNotEqual(readline.readline_version(), 0)
@@ -24,4 +48,29 @@ class ReadlineTests(unittest.TestCase):
 
     def test_parse_and_bind_empty_string(self):
         self.assertEqual(readline.parse_and_bind(''), None)
+
+    def test_read_initfile_None(self):
+        self.mkfile('my_init')
+        self.mkfile(self.initfile)
+        readline.read_init_file(None)
+
+    def test_read_initfile_empty_string(self):
+        self.mkfile('my_init')
+        self.mkfile(self.initfile)
+        self.assertEqual(readline.read_init_file(''), None)
+
+    def test_read_history_file_None(self):
+        self.mkfile(self.histfile)
+        readline.read_history_file(None)
+
+    def test_read_history_file_empty_string(self):
+        self.mkfile(self.histfile)
+        self.assertRaises(IOError, readline.read_history_file, '')
+
+    def test_write_history_file_None(self):
+        readline.write_history_file(None)
+        self.assertTrue(isfile(self.histfile))
+
+    def test_write_history_file_empty_string(self):
+        self.assertRaises(IOError, readline.write_history_file, '')
 
