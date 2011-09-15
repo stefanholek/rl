20045f776eecc350275469138986762e3d71945e
Stefan H. Holek <stefan@epy.co.at>
2011-09-15 18:40:16 +0200

    Document functions last.

--- rl/__init__.py
+++ rl/__init__.py
@@ -13,6 +13,17 @@ rl exports the following components:
     Interface to the active readline completion. Used to interact
     with readline when a completion is in progress.
 
+:obj:`rl.history <rl.History>`
+    Interface to the readline history. Used to
+    manipulate history entries and to read and write history files.
+
+:mod:`rl.readline <rl.readline>`
+    The readline bindings module. Contains everything known from the standard
+    library plus extensions specific to the rl package.  The :obj:`completer <rl.Completer>`,
+    :obj:`completion <rl.Completion>`, and :obj:`history <rl.History>`
+    interfaces make use of this module, and you should rarely need to interact
+    with it directly.
+
 :func:`rl.generator() <rl.generator>`
     A factory turning any callable into a *completion entry function* that
     can be handed to readline.
@@ -21,17 +32,6 @@ rl exports the following components:
     A decorator printing exceptions to stderr. Useful when writing Python
     completions and hooks, as exceptions occurring there are usually
     swallowed by the in-between C code.
-
-:obj:`rl.history <rl.History>`
-    Interface to the readline history. Used to read and write history files
-    and to manipulate history entries.
-
-:mod:`rl.readline <rl.readline>`
-    The readline bindings module. Contains everything known from the standard
-    library plus extensions specific to the rl package.  The :obj:`completer <rl.Completer>`,
-    :obj:`completion <rl.Completion>`, and :obj:`history <rl.History>`
-    interfaces make use of this module, and you should rarely need to interact
-    with it directly.
 
 Readline Completion
 ===================
