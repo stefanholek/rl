d4cd20d99d4d54f651e731952eec8347505d8a9c
Stefan H. Holek <stefan@epy.co.at>
2012-07-14 10:33:48 +0200

    Implement history iterators in C.

--- rl/readline.c
+++ rl/readline.c
@@ -47,6 +47,7 @@
 #if (PY_MAJOR_VERSION >= 3)
 #define PyInt_FromLong PyLong_FromLong
 #define PyInt_AsLong PyLong_AsLong
+#define PyInt_FromSsize_t PyLong_FromSsize_t
 #define PyString_FromString PyUnicode_DECODE
 #endif
 
@@ -745,6 +746,38 @@ PyDoc_STRVAR(doc_get_history_list,
 "get_history_list() -> list\n\
 Return the entire history as a Python list. \
 Element 0 of the list is the beginning of time.");
+
+
+/* Exported function returning an iterator over the history */
+
+static PyObject *hist_iter(PyObject *);
+
+
+static PyObject *
+get_history_iter(PyObject *self, PyObject *noarg)
+{
+	return hist_iter(NULL);
+}
+
+PyDoc_STRVAR(doc_get_history_iter,
+"get_history_iter() -> historyiterator\n\
+Return a forward iterator over the history (oldest to newest).");
+
+
+/* Exported function returning a reverse iterator over the history */
+
+static PyObject *hist_reversed(PyObject *, PyObject *);
+
+
+static PyObject *
+get_history_reviter(PyObject *self, PyObject *noarg)
+{
+	return hist_reversed(NULL, NULL);
+}
+
+PyDoc_STRVAR(doc_get_history_reviter,
+"get_history_reviter() -> historyreverseiterator\n\
+Return a reverse iterator over the history (newest to oldest).");
 
 
 /* Exported function to get current length of history */
@@ -2541,6 +2574,10 @@ static struct PyMethodDef readline_methods[] =
 	 METH_NOARGS, doc_history_is_stifled},
 	{"get_history_list", get_history_list,
 	 METH_NOARGS, doc_get_history_list},
+	{"get_history_iter", get_history_iter,
+	 METH_NOARGS, doc_get_history_iter},
+	{"get_history_reviter", get_history_reviter,
+	 METH_NOARGS, doc_get_history_reviter},
 	/* </rl.readline> */
 
 	{0, 0}
@@ -3025,3 +3062,244 @@ initreadline(void)
 	setup_readline();
 }
 #endif
+
+
+/*********************** History Iterator **************************/
+
+typedef struct {
+    PyObject_HEAD
+    long it_index;
+    HIST_ENTRY **it_seq; /* Set to NULL when iterator is exhausted */
+} histiterobject;
+
+static PyObject *hist_iter(PyObject *);
+static void histiter_dealloc(histiterobject *);
+static int histiter_traverse(histiterobject *, visitproc, void *);
+static PyObject *histiter_next(histiterobject *);
+static PyObject *histiter_len(histiterobject *);
+
+PyDoc_STRVAR(length_hint_doc, "Private method returning an estimate of len(list(it)).");
+
+static PyMethodDef histiter_methods[] = {
+    {"__length_hint__", (PyCFunction)histiter_len, METH_NOARGS, length_hint_doc},
+    {NULL,              NULL}           /* sentinel */
+};
+
+PyTypeObject PyHistIter_Type = {
+    PyVarObject_HEAD_INIT(&PyType_Type, 0)
+    "historyiterator",                          /* tp_name */
+    sizeof(histiterobject),                     /* tp_basicsize */
+    0,                                          /* tp_itemsize */
+    /* methods */
+    (destructor)histiter_dealloc,               /* tp_dealloc */
+    0,                                          /* tp_print */
+    0,                                          /* tp_getattr */
+    0,                                          /* tp_setattr */
+    0,                                          /* tp_compare */
+    0,                                          /* tp_repr */
+    0,                                          /* tp_as_number */
+    0,                                          /* tp_as_sequence */
+    0,                                          /* tp_as_mapping */
+    0,                                          /* tp_hash */
+    0,                                          /* tp_call */
+    0,                                          /* tp_str */
+    PyObject_GenericGetAttr,                    /* tp_getattro */
+    0,                                          /* tp_setattro */
+    0,                                          /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC,    /* tp_flags */
+    0,                                          /* tp_doc */
+    (traverseproc)histiter_traverse,            /* tp_traverse */
+    0,                                          /* tp_clear */
+    0,                                          /* tp_richcompare */
+    0,                                          /* tp_weaklistoffset */
+    PyObject_SelfIter,                          /* tp_iter */
+    (iternextfunc)histiter_next,                /* tp_iternext */
+    histiter_methods,                           /* tp_methods */
+    0,                                          /* tp_members */
+};
+
+
+static PyObject *
+hist_iter(PyObject *seq)
+{
+    histiterobject *it;
+
+    it = PyObject_GC_New(histiterobject, &PyHistIter_Type);
+    if (it == NULL)
+        return NULL;
+    it->it_index = 0;
+    it->it_seq = history_list();
+    _PyObject_GC_TRACK(it);
+    return (PyObject *)it;
+}
+
+static void
+histiter_dealloc(histiterobject *it)
+{
+    _PyObject_GC_UNTRACK(it);
+    PyObject_GC_Del(it);
+}
+
+static int
+histiter_traverse(histiterobject *it, visitproc visit, void *arg)
+{
+    Py_VISIT(it->it_seq);
+    return 0;
+}
+
+static PyObject *
+histiter_next(histiterobject *it)
+{
+    Py_ssize_t index;
+    HIST_ENTRY **seq;
+    PyObject *item;
+
+    assert(it != NULL);
+    if (it->it_seq == NULL)
+        return NULL;
+    index = it->it_index;
+    seq = history_list();
+
+    if (index < history_length) {
+        item = PyString_FromString(seq[index]->line);
+        if (item == NULL)
+            goto error;
+        ++it->it_index;
+        return item;
+    }
+error:
+    it->it_seq = NULL;
+    return NULL;
+}
+
+static PyObject *
+histiter_len(histiterobject *it)
+{
+    Py_ssize_t len;
+    if (it->it_seq != NULL) {
+        len = history_length - it->it_index;
+        if (len >= 0)
+            return PyInt_FromSsize_t(len);
+    }
+    return PyInt_FromLong(0);
+}
+
+
+/*********************** History Reverse Iterator **************************/
+
+typedef struct {
+    PyObject_HEAD
+    Py_ssize_t it_index;
+    HIST_ENTRY **it_seq; /* Set to NULL when iterator is exhausted */
+} histreviterobject;
+
+static PyObject *hist_reversed(PyObject *, PyObject *);
+static void histreviter_dealloc(histreviterobject *);
+static int histreviter_traverse(histreviterobject *, visitproc, void *);
+static PyObject *histreviter_next(histreviterobject *);
+static PyObject *histreviter_len(histreviterobject *);
+
+static PyMethodDef histreviter_methods[] = {
+    {"__length_hint__", (PyCFunction)histreviter_len, METH_NOARGS, length_hint_doc},
+    {NULL,              NULL}           /* sentinel */
+};
+
+PyTypeObject PyHistRevIter_Type = {
+    PyVarObject_HEAD_INIT(&PyType_Type, 0)
+    "historyreverseiterator",                   /* tp_name */
+    sizeof(histreviterobject),                  /* tp_basicsize */
+    0,                                          /* tp_itemsize */
+    /* methods */
+    (destructor)histreviter_dealloc,            /* tp_dealloc */
+    0,                                          /* tp_print */
+    0,                                          /* tp_getattr */
+    0,                                          /* tp_setattr */
+    0,                                          /* tp_compare */
+    0,                                          /* tp_repr */
+    0,                                          /* tp_as_number */
+    0,                                          /* tp_as_sequence */
+    0,                                          /* tp_as_mapping */
+    0,                                          /* tp_hash */
+    0,                                          /* tp_call */
+    0,                                          /* tp_str */
+    PyObject_GenericGetAttr,                    /* tp_getattro */
+    0,                                          /* tp_setattro */
+    0,                                          /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC,    /* tp_flags */
+    0,                                          /* tp_doc */
+    (traverseproc)histreviter_traverse,         /* tp_traverse */
+    0,                                          /* tp_clear */
+    0,                                          /* tp_richcompare */
+    0,                                          /* tp_weaklistoffset */
+    PyObject_SelfIter,                          /* tp_iter */
+    (iternextfunc)histreviter_next,             /* tp_iternext */
+    histreviter_methods,                        /* tp_methods */
+    0,
+};
+
+
+static PyObject *
+hist_reversed(PyObject *seq, PyObject *unused)
+{
+    histreviterobject *it;
+
+    it = PyObject_GC_New(histreviterobject, &PyHistRevIter_Type);
+    if (it == NULL)
+        return NULL;
+    it->it_index = history_length - 1;
+    it->it_seq = history_list();
+    PyObject_GC_Track(it);
+    return (PyObject *)it;
+}
+
+static void
+histreviter_dealloc(histreviterobject *it)
+{
+    PyObject_GC_UnTrack(it);
+    PyObject_GC_Del(it);
+}
+
+static int
+histreviter_traverse(histreviterobject *it, visitproc visit, void *arg)
+{
+    Py_VISIT(it->it_seq);
+    return 0;
+}
+
+static PyObject *
+histreviter_next(histreviterobject *it)
+{
+    Py_ssize_t index;
+    HIST_ENTRY **seq;
+    PyObject *item;
+
+    assert(it != NULL);
+    if (it->it_seq == NULL)
+        return NULL;
+    index = it->it_index;
+    seq = history_list();
+
+    if (index >= 0 && index < history_length) {
+        item = PyString_FromString(seq[index]->line);
+        if (item == NULL)
+            goto error;
+        it->it_index--;
+        return item;
+    }
+error:
+    it->it_seq = NULL;
+    return NULL;
+}
+
+static PyObject *
+histreviter_len(histreviterobject *it)
+{
+    Py_ssize_t len;
+    if (it->it_seq != NULL) {
+        len = it->it_index + 1;
+        if (len >= 0 && len <= history_length)
+            return PyInt_FromSsize_t(len);
+    }
+    return PyInt_FromLong(0);
+}
+
