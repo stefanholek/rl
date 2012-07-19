dde24ddf1b9da37cd353f6bb6df0fbaa2a8d9a11
Stefan H. Holek <stefan@epy.co.at>
2012-07-19 18:08:45 +0200

    Disuse Unicode functions deprecated in Python 3.3.

--- rl/unicode.c
+++ rl/unicode.c
@@ -1,15 +1,14 @@
 #include "Python.h"
 #include "unicode.h"
 
-
-/* Unicode support */
-
 #if (PY_MAJOR_VERSION >= 3)
 
 /* See PEP 383 */
 #define _ENCODING Py_FileSystemDefaultEncoding
 #define _ERRORS "surrogateescape"
 
+
+/* Unicode support */
 
 PyObject *
 PyUnicode_DECODE(const char *text)
@@ -37,7 +36,11 @@ PyUnicode_INDEX(const char *text, Py_ssize_t index)
 	u = PyUnicode_Decode(text, index, _ENCODING, _ERRORS);
 	if (u == NULL)
 		return -1;
+#if (PY_VERSION_HEX >= 0x03030000)
+	i = PyUnicode_GET_LENGTH(u);
+#else
 	i = PyUnicode_GET_SIZE(u);
+#endif
 	Py_DECREF(u);
 	return i;
 }
