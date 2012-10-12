a2d45e28ea76c156cf7b70ddef7aa5c3dbce1d4b
Stefan H. Holek <stefan@epy.co.at>
2012-10-13 01:38:38 +0200

    Python 3.3 has been released.

--- docs/conf.py
+++ docs/conf.py
@@ -221,6 +221,6 @@ man_pages = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'py': ('http://docs.python.org', 'http://docs.python.org/2.7/objects.inv'),
-    'py3k': ('http://docs.python.org/py3k', 'http://docs.python.org/3.2/objects.inv'),
+    'py3k': ('http://docs.python.org/py3k', 'http://docs.python.org/3.3/objects.inv'),
 }
 
