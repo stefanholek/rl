8d443d0378b76b884b669a24b77305dcd04b695f
Stefan H. Holek <stefan@epy.co.at>
2017-02-18 16:44:35 +0100

    Silence compiler warning.

--- rl/readline.c
+++ rl/readline.c
@@ -2837,7 +2837,7 @@ rlhandler(char *text)
 extern PyThreadState* _PyOS_ReadlineTState;
 
 static char *
-readline_until_enter_or_signal(char *prompt, int *signal)
+readline_until_enter_or_signal(const char *prompt, int *signal)
 {
 	char * not_done_reading = "";
 	fd_set selectset;
@@ -2909,7 +2909,7 @@ onintr(int sig)
 
 
 static char *
-readline_until_enter_or_signal(char *prompt, int *signal)
+readline_until_enter_or_signal(const char *prompt, int *signal)
 {
 	PyOS_sighandler_t old_inthandler;
 	char *p;
@@ -2936,7 +2936,7 @@ readline_until_enter_or_signal(char *prompt, int *signal)
 
 
 static char *
-call_readline(FILE *sys_stdin, FILE *sys_stdout, char *prompt)
+call_readline(FILE *sys_stdin, FILE *sys_stdout, const char *prompt)
 {
 	size_t n;
 	char *p, *q;
