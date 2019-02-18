979960b768c509160134522c8349673e4bd45d11
Stefan H. Holek <stefan@epy.co.at>
2019-02-18 09:50:33 +0100

    Use RTD theme for docs.

--- docs/conf.py
+++ docs/conf.py
@@ -93,17 +93,21 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #html_theme = 'nature'
 #html_style = 'nature-plus.css'
-html_theme = 'classic'
-html_style = 'classic-plus.css'
-html_theme_options = {'sidebarwidth': 250}
+#html_theme = 'classic'
+#html_style = 'classic-plus.css'
+#html_theme_options = {'sidebarwidth': 250}
 
-#try:
-#    import sphinx_rtd_theme
-#except ImportError:
-#    pass
-#else:
-#    html_theme = 'sphinx_rtd_theme'
-#    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    pass
+else:
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_style = 'theme-plus.css'
+
+if os.environ.get('READTHEDOCS', '') == 'True':
+    html_style = 'theme-plus.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
