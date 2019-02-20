3c55a84e0f153b4d7da854defac0401bd82f69e8
Stefan H. Holek <stefan@epy.co.at>
2019-02-20 23:35:59 +0100

    Disable a test that produces output.

--- rl/tests/test_callhooks.py
+++ rl/tests/test_callhooks.py
@@ -187,7 +187,7 @@ class DisplayMatchesHookTests(JailSetup):
         self.assertEqual(called, [('fred.', ['fred.gif', 'fred.txt'], 8)])
 
     # FIXME: Prints to rl_outstream
-    def test_default_display_matches_hook(self):
+    def DISABLED_test_default_display_matches_hook(self):
         self.mkfile('fred.txt', 'fred.gif')
         completer.completer = filecomplete
         readline.complete_internal('?')
