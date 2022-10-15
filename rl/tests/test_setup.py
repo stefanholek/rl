99e242f1fb9d613032d871b25e6dcdf43de61f8f
Stefan H. Holek <stefan@epy.co.at>
2022-10-15 10:41:59 +0200

    test_sort_attr fails in Python >= 3.8.

--- rl/tests/test_setup.py
+++ rl/tests/test_setup.py
@@ -9,8 +9,15 @@ class SortTests(unittest.TestCase):
         l.sort(key=lambda x: len(x))
         self.assertEqual(l, ['foo.so', 'foo.so.6', 'foo.so.6.3'])
 
-    def test_sort_attr(self):
-        l = ['foo.so.6.3', 'foo.so.6', 'foo.so']
-        l.sort(key=operator.attrgetter('__len__'))
-        self.assertEqual(l, ['foo.so', 'foo.so.6', 'foo.so.6.3'])
+    # Stopped working in Python 3.8
+    #
+    # Traceback (most recent call last):
+    #   File "/Users/stefan/sandbox/rl/rl/tests/test_setup.py", line 14, in test_sort_attr
+    #     l.sort(key=operator.attrgetter('__len__'))
+    # TypeError: '<' not supported between instances of 'method-wrapper' and 'method-wrapper'
+    #
+    #def test_sort_attr(self):
+    #    l = ['foo.so.6.3', 'foo.so.6', 'foo.so']
+    #    l.sort(key=operator.attrgetter('__len__'))
+    #    self.assertEqual(l, ['foo.so', 'foo.so.6', 'foo.so.6.3'])
 
